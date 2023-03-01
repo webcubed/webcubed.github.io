e4df86acf205dc24842e5bc9569450bb5293d2f2
oofish8888 <77589042+oofish8888@users.noreply.github.com>
2023-03-01 12:36:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ window.onload = function() {
   };
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  initializeApp(firebaseConfig);
   // This is very IMPORTANT!! We're going to use "db" a lot.
   var db = firebase.database()
   // We're going to use oBjEcT OrIeNtEd PrOgRaMmInG. Lol
