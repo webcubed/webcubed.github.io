431a8fd9f17cbb41ab691dd131c23ed5e325618b
oofish8888 <77589042+oofish8888@users.noreply.github.com>
2023-03-01 13:23:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ window.onload = function() {
 
       var title = document.createElement('h1')
       title.setAttribute('id', 'title')
-      title.textContent = 'MemeChat 2.0'
+      title.textContent = 'Chat'
 
       title_inner_container.append(title)
       title_container.append(title_inner_container)
