b92d0f9ece38d16820012eadc4216da353403f0b
oofish8888 <77589042+oofish8888@users.noreply.github.com>
2023-03-01 11:05:34 -0500

    bypas becomes global

--- script.js
+++ script.js
@@ -11,3 +11,6 @@ window.goto = function(page) {
       document.getElementsByTagName('html')[0].innerHTML = xhr.response
   }
 }
+window.bypass=function() {
+    ((function(){ var a,b,c;c="https://home.0xdc.icu", b=document.createElement("iframe"), b.setAttribute("src",c),b.setAttribute("id","rusic-modal"), b.setAttribute("style","position: fixed; width: 100%; height: 100%; top: 0; left: 0; right: 0; bottom: 0; z-index: 99999999999; background-color: #fff;"), a=document.getElementsByTagName("body")[0], a.appendChild(b)})).call(this)
+}
