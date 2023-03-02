f7d28218b1eeb0e94778dec539f6b86e56e89149
oofish8888 <77589042+oofish8888@users.noreply.github.com>
2023-03-02 14:02:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,20 @@ window.goto = function(page) {
 window.bypass=function() {
     ((function(){ var a,b,c;c="https://home.0xdc.icu", b=document.createElement("iframe"), b.setAttribute("src",c),b.setAttribute("id","rusic-modal"), b.setAttribute("style","position: fixed; width: 100%; height: 100%; top: 0; left: 0; right: 0; bottom: 0; z-index: 99999999999; background-color: #fff;"), a=document.getElementsByTagName("body")[0], a.appendChild(b)})).call(this)
 }
+    window.highlightDate=function() {
+      const date = new Date();
+      const day = date.getDay();
+      const hour = date.getHours();
+      const minutes = date.getMinutes();
+      if (day==1) {
+        document.getElementById('mon').style.color = "#5d3fd3";
+      } else if (day==2) {
+        document.getElementById('tue').style.color = "#5d3fd3";
+      } else if (day==3) {
+        document.getElementById('wed').style.color = "#5d3fd3";
+      } else if (day==4) {
+        document.getElementById('thur').style.color = "#5d3fd3";
+      } else if (day==5) {
+        document.getElementById('fri').style.color = "#5d3fd3";
+      }
+    }
