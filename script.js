1b9b012f380e0e228db8db5f480997a1d0e56cf3
oofish8888 <77589042+oofish8888@users.noreply.github.com>
2023-03-03 10:01:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-const ipifyURL = "https://api.ipify.org";
-async function grabIp() {
-  try {
-    const response = await fetch(ipifyURL);
-    const ipAddress = await response.text();
-    window.ip = ipAddress;
-     return ipAddress;
-  } catch (error) {
-    console.error(error);
-  }
-}
-grabIp()
-window.goto = function(page) {
-  const nextURL = 'https://idabest.tk/' + page;
-  const nextTitle = 'idabest.tk';
-  const nextState = { nextPage: page };
-  window.history.pushState(nextState, nextTitle, nextURL);
-  window.history.replaceState(nextState, nextTitle, nextURL);
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://idabest.tk/' + page, true);
-  xhr.send();
-  xhr.onload=function() {
-      document.getElementsByTagName('html')[0].innerHTML = xhr.response
-  }
-}
-window.bypass=function() {
-    ((function(){ var a,b,c;c="https://home.0xdc.icu", b=document.createElement("iframe"), b.setAttribute("src",c),b.setAttribute("id","rusic-modal"), b.setAttribute("style","position: fixed; width: 100%; height: 100%; top: 0; left: 0; right: 0; bottom: 0; z-index: 99999999999; background-color: #fff;"), a=document.getElementsByTagName("body")[0], a.appendChild(b)})).call(this)
-}
 window.highlightDate=function() {
   const date = new Date();
   const day = date.getDay();
@@ -42,4 +14,41 @@ window.highlightDate=function() {
   } else if (day==5) {
     document.getElementById('fri').style.color = "#5d3fd3";
   }
+}
+
+const ipifyURL = "https://api.ipify.org";
+async function grabIp() {
+  try {
+    const response = await fetch(ipifyURL);
+    const ipAddress = await response.text();
+    window.ip = ipAddress;
+     return ipAddress;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+grabIp()
+
+window.goto = function(page) {
+  const nextURL = 'https://idabest.tk/' + page;
+  const nextTitle = 'idabest.tk';
+  const nextState = { nextPage: page };
+  window.history.pushState(nextState, nextTitle, nextURL);
+  window.history.replaceState(nextState, nextTitle, nextURL);
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://idabest.tk/' + page, true);
+  xhr.send();
+  xhr.onload=function() {
+      document.getElementsByTagName('html')[0].innerHTML = xhr.response
+  }
+  if(page==='schedule') {
+    setTimeout(() => {
+      highlightDate();
+    }, 300);
+  }
+}
+
+window.bypass=function() {
+    ((function(){ var a,b,c;c="https://home.0xdc.icu", b=document.createElement("iframe"), b.setAttribute("src",c),b.setAttribute("id","rusic-modal"), b.setAttribute("style","position: fixed; width: 100%; height: 100%; top: 0; left: 0; right: 0; bottom: 0; z-index: 99999999999; background-color: #fff;"), a=document.getElementsByTagName("body")[0], a.appendChild(b)})).call(this)
 }
