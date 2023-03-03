492a9c370afda3706db06de8e6f4f63c1f60bec4
oofish8888 <77589042+oofish8888@users.noreply.github.com>
2023-03-03 12:29:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -207,7 +207,7 @@ window.onload = function() {
             admin_panel.append(user_names);
           });
         })
-        document.body.append(admin_panel)
+        chat_container.body.append(admin_panel)
       }
       
 
