82e0aaecdbf6281e536c36c402c31b4524e03be7
oofish8888 <77589042+oofish8888@users.noreply.github.com>
2023-03-29 09:25:36 -0400

    add a random comment because no life

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // This is very skidded
 window.onload = function() {
-  localStorage.clear()
+  localStorage.clear() // log out everyone on start lMFAOOAOAOOOO
   const firebaseConfig = {
     apiKey: "AIzaSyC9IeopdU5LsK2T38yqDtm29zesMRAecfk",
     authDomain: "idabest-c1ad9.firebaseapp.com",
