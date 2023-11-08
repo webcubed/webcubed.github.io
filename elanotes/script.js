3ec374148b5f6c174143186ccae5e78fabaedc40
webcubed <lainathannlaiv2@gmail.com>
2023-11-07 21:11:51 -0500

    test

---
+++ elanotes/script.js
@@ -0,0 +1,188 @@
+window.onload = function () { 
+    document.getElementById("content").innerHTML = marked.parse(`
+# Test review notes 😯
+---
+## From page 5:
+--- 
+Learn how to analyze character traits 😯
+- Remember these 5: exposition, rising action, climax, falling action, and resolution (ERCFR)
+- Some stories have flashbacks. (self-explanatory) and they interrupt the story’s chronological order.
+- Now, more relevant stuff: 
+- Character traits include Physical appearance, Speech, thoughts, and actions, other’s impressions of the character, and their interactions.\n
+## From page 18:
+--- 
+Vocabulary, suffixes, word stuff.
+- A suffix is a word part that appears at the end of a root or base word to form a new comment.\n
+## From page 19: 
+--- 
+“Watch your language.”\n
+- Learn what dialogue is: \n
+    Use quotation marks to enclose a direct quotation —a character's exact words.\n
+
+            "Let me read your diary."\n
+- Direct quotations usually begin with an uppercase letter. However, when the speaker interrupts a quoted sentence, the second part of the quotation begins with a lowercase letter.
+
+           "By two minutes," said Jenny, "and anyway, maybe I'm not the oldest anymore."\n
+- A period or a comma is always placed inside the closing quotation marks. The same is true of a question mark or exclamation point unless it is not part of the quotation. \n
+            "Mrs. Jarred," said a female reporter with a microphone, "has science gone too far?"\n
+## From page 21: \n
+--- 
+Determining the author’s purpose.\n
+- This is self-explanatory as well, so allow me to provide some examples:\n
+- Some examples of “to inform or explain” include Encyclopedia entries, informative articles, how-to articles, and Biographies.\n
+      - “To persuade” includes editorials, opinion essays/blogs, advertisements
+      - “To entertain” includes stories, novels, some essays.
+      - “To express thought or feelings” (kind of opinion but HMH thinks otherwise) includes Poems, personal essays, and journals.
+- Oh and, make sure you know how to cite evidence. It can:
+    - Support an analysis of a text. Evidence is not limited to, facts, definitions, quotations, and concrete details. Mix with your own knowledge = perfect explanation.\n
+## From page 30:
+--- 
+Some vocabulary expansion and whatnot \n
+Refrence resources: e.g. dictionaries \n
+- A dictionary is a valuable resource for checking definitions and expanding your vocabulary. Searching and browsing methods differ for print and digital glossaries, but users can find the same basic information about each entry word.\n
+• pronunciation
+• part of speech
+• one or more definitions
+• etymology (word origin or
+history)\n
+## From page 31:
+--- 
+“Watch your language” again\n
+- Correlative Conjunctions (ooh, fancy) 
+- Writers rely on single-word conjunctions including as, and, but, and if to link ideas.
+- Correlative conjunctions are pairs of words that connect words, phrases, and clauses.
+- Examples: 
+    - either / or
+    - neither / nor
+    - both / and
+    - no sooner / than
+    - whether / or
+- The two parts of a correlative conjunction need to connect similar parts of speech, phrases, or clauses. 
+- A noun must connect to a noun, a prepositional phrase must connect to a prepositional phrase, and so on. This helps to maintain a parallel structure.\n
+## From page 33: \n
+--- \n
+Analyze folktales and humor\n
+- In some folktales, the main character is a trickster—a character who goes against acceptable behavior and attempts to fool another character, often for selfish reasons.\n
+- Tricksters may succeed or fail, depending on whether their trickery is discovered.\n
+- Writers use humor to amuse readers (wow didn’t know that)\n
+- Readers usually Infer what is humorous about the character’s actions. \n
+- Apparently they do so by connecting them with their own experiences.\n
+- Humor can come from: plot events, character words, or language used to write story.\n
+## From page 40: \n
+--- \n
+How to use glossary 101\n
+- A glossary is a list of specialized terms and their definitions. They are in alphabetic order, in the back of the book.
+- Format: Entry word: Pronunciation, part of speech, and definition (kinda like dictionary)\n
+## From page 40:
+--- 
+“Let’s eat grandma!”\n
+- Commas. Commas are apparently very important because it can decide the fate of grandma. \n
+- Commas show readers which words or phrases go together and indicate which part of a sentence is most important somehow.\n
+- Here are some use case examples: \n
+    - To set off introductory words or phrases:\n
+
+        “Wife, come, cook these as quickly as you can. On hearing this, Goha sat bolt upright.”\n
+To set off nonessential words, phrases, and clauses:\n
+
+    “Just to be sure they're done, of course. Just a taste.”\n
+\n
+- To separate dependent clauses from independent clauses:\n
+\n
+    “One day, a hungry man named Goha was walking in the marketplace, his mind on the dinner to come, when he chanced to pass a butcher's shop.”\n
+
+to separate two independent clauses joined by the\n
+coordinating conjunctions and, but, or, for, nor, so, or yet:\n
+\n
+    “The butcher came out to see who had died, but by now such a crowd had gathered that he could see nothing.”\n
+\n
+## From page 44: 
+--- 
+Analyze rhyme, rhyme scheme, and mood.\n
+### Note: the following are completely copied off of HMH’s textbook.\n
+- Rhyme is the repetition of sounds at the ends of words. Words rhyme when their accented vowels and the letters that follow them create identical or similar sounds.\n
+- Examples: \n
+    - dreary / weary\n
+    - more / roar\n
+    - chair / stare\n
+- Poets use rhyme for a number of purposes:\n
+    - to create a musical quality and to emphasize sounds that suggest particular feelings, such as surprise or sadness\n
+    - to create rhythms, or patterns of stressed and unstressed syllables\n
+- Rhyme scheme: A rhyme scheme is a pattern of end rhymes in a poem.\n
+- A rhyme scheme is noted by assigning a letter of the alphabet, starting with a, to each end rhyme, as in the example from the first stanza of "Eldorado."\n
+### RHYME SCHEME example: \n
+    Gaily bedight, a\n
+    A gallant knight, a\n
+    In sunshine and in shadow, b\n
+    Had journeyed long, c\n
+    Singing a song, c\n
+    In search of Eldorado. b\n
+- Mood = the feeling the poem creates for the reader. Some words that might describe the mood of a poem are:
+    - cheerful, 
+    - regretful, 
+    - mysterious, 
+    - or humorous.
+## From page 45:\n
+--- \n
+Analyze sound devices & mood\n
+- Rhyme and sound devices are allegedly used to convey a poem’s meaning & mood.\n
+- Alliteration: repetition of beginning consonant sounds in words. Example: “And when white moths …”\n
+- Assonance: repetition of vowel sounds in non-rhyming words. Example: “Though I am old …” apparently you need to look at the “ou” of though and the “o” of old.\n
+- Consonance: Repetition of consonant sounds within and at ends of words. Example: “When I had Laid” (look at the end of had and laid [d])\n
+- Repetition: Self explanatory: Repeating the same words or phrases, or the same grammatical construction. Example: “apple blossom, apples of the moon, apples of the sun” (“apple”)\n
+## From page 58: \n
+--- \n
+Analyze narrator & graphic novels.\n
+- A narrator tells the story and shapes the point of view for the reader. The three types of point of view:\n
+- First person: narrator is a story character, narrator reveals only what he or she knows and experiences, and narrator makes the story feel personal.\n
+- Third person limited: narrator is not a story character, and narrator reveals the experiences, thoughts, and feelings of one character\n
+- Third person omniscient: narrator is not a story character, narrator reveals the thoughts, feelings, and experiences of all characters, and narrator may give extra insight into the story or characters\n
+- First person uses “I, me, my, etc.”\n
+- Both third person perspectives uses “He, She, They” (those are probably not pronouns)\n
+- Graphic novels contain characters, plot, setting, theme, and point of view. They tell story through image and captions, they include Image panels: illustrations in a border or “frame” They also include Captions, which are boxes of text that narrate the story, Speech balloons Graphics that show dialogue, usually in the shape of a 🎈.Thought bubbles (self-explanatory) cloud-shaped graphics that show character thoughts.\n
+## From page 80: 
+--- 
+Word origins
+A word's history and origin can help you learn its meaning and connotation. 
+Example: Spectator comes from the Latin spectare, meaning "to watch." This word part is found in other English words, such as spectacle. The origin suggests that the word has to do with seeing or watching, and it has a neutral connotation.\n
+## From page 81: \n
+--- \n
+“Watch your language” \n
+- Complex Sentences: Subordinating Conjunctions
+    - A complex sentence consists of an independent clause and at least one subordinate clause. 
+    - Subordinate clauses are clauses that contain subjects and verbs but do not form complete sentences. Subordinating conjunctions are conjunctions that introduce subordinate clauses.
+- Subordinating conjunctions can:
+    - provide transitions between ideas to show place, time, cause-effect relationships, or conditional relationships
+    - show that one idea in a sentence is more important than another idea
+- Examples:
+    - after
+    - although
+    - as
+    - because
+    - before
+    - even though
+    - if
+    - once
+    - since
+    - so that
+    - though
+    - unless
+    - until
+    - when
+    - where
+    - While\n
+## From page 101: \n
+--- 
+- Analyze character and conflict.\n
+- Characters are the people, animals, or other figures in a story. Analyzing characters' traits and motivations helps you understand both the characters and the story.\n
+- Character traits are a character's personal qualities\n
+- Character motivations are the reasons characters act the way they do\n
+- Every story is built upon a conflict, or a struggle between opposing forces. Two types of conflicts often appear in stories.\n
+• An external conflict is a struggle against an outside force, such\n
+as nature, a physical obstacle, or another character.\n
+• An internal conflict is a struggle that occurs in a character's mind, often due to a clash in feelings, thoughts, or values.\n
+A story's setting —the time and place of the action —may also affect character development.\n
+\n
+# CONGRATS! YOU WILL NOW PROCEED TO GET a 45% on the TEST!\n
+# PROPS TO HMH FOR MAKING THIS SUCH AN IMPOSSIBLE TASK!\n
+`);
+}
