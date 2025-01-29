b405c2fa88033a2b1dd2c6d69cc8826cc24bf0af
webcubed from home <lainathannlaiv2@gmail.com>
2025-01-29 12:51:14 -0500

    change defaults

--- numgame/script.js
+++ numgame/script.js
@@ -1,4 +1,4 @@
-let allowedDigits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let allowedDigits = [1, 2, 3, 4, 5, 6];
 class game {
     constructor(allowedDigits, numberLength) {
         this.allowedDigits = allowedDigits;
