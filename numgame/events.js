6924759b78534159ba207d20f7ce31c51b3d3205
webcubed from home <lainathannlaiv2@gmail.com>
2025-02-01 21:19:20 -0500

    Calculation of possible solutions does not account for disallowed reprats Fixes #1

--- numgame/events.js
+++ numgame/events.js
@@ -6,5 +6,13 @@ document.addEventListener("DOMContentLoaded", () => {
 		} else {
 			document.querySelector("#customnumberselection").style.display = "none";
 		}
+
+		if (configValues.shownumpossiblecombinations) {
+			document.querySelector("#possiblecombinations").textContent =
+				"Possible Combinations: " + game.calculatePossibleCombinations();
+		} else {
+			document.querySelector("#possiblecombinations").textContent =
+				"Possible Combinations: N/A";
+		}
 	});
 });
