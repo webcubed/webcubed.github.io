1dd95e2455781f180f3233e1de5d486ba7b88f9f
webcubed on laptop <no>
2025-03-10 11:57:41 -0400

    test

--- smh/script.js
+++ smh/script.js
@@ -1,3 +1,29 @@
+// eslint-disable-next-line no-unused-vars
+async function promptForUpdate() {
+	const dialog = document.createElement("dialog");
+	dialog.innerHTML = `
+	<p>A new version of this site is available. Reload to update?</p>
+	<button id="confirm">Confirm</button>
+	<button id="cancel">Cancel</button>
+	`;
+	document.body.append(dialog);
+	dialog.showModal();
+
+	const confirmButton = dialog.querySelector("#confirm");
+	const cancelButton = dialog.querySelector("#cancel");
+	return new Promise((resolve) => {
+		confirmButton.addEventListener("click", () => {
+			dialog.close();
+			resolve(true);
+		});
+
+		cancelButton.addEventListener("click", () => {
+			dialog.close();
+			resolve(false);
+		});
+	});
+}
+
 document.addEventListener("DOMContentLoaded", function () {
 	document.querySelector("#idinput").value =
 		"1fc79e20-1483-4528-854d-cbb776111132"; // Update here (find "refId" in parent of fetch)
