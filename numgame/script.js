6ea6b6958dfea57500e11a46c9256ff51c903131
webcubed from home <lainathannlaiv2@gmail.com>
2025-03-10 18:28:23 -0400

    now we can remove redundant promptforupdate function in numgame

--- numgame/script.js
+++ numgame/script.js
@@ -29,32 +29,6 @@ function factorial(n) {
 	return f[n];
 }
 
-// eslint-disable-next-line no-unused-vars
-function promptForUpdate() {
-	const dialog = document.createElement("dialog");
-	dialog.innerHTML = `
-	<p>A new version of this site is available. Reload to update?</p>
-	<button id="confirm">Confirm</button>
-	<button id="cancel">Cancel</button>
-	`;
-	document.body.append(dialog);
-	dialog.showModal();
-
-	const confirmButton = dialog.querySelector("#confirm");
-	const cancelButton = dialog.querySelector("#cancel");
-	return new Promise((resolve) => {
-		confirmButton.addEventListener("click", () => {
-			dialog.close();
-			resolve(true);
-		});
-
-		cancelButton.addEventListener("click", () => {
-			dialog.close();
-			resolve(false);
-		});
-	});
-}
-
 // Let page work offline
 /*
 if ("serviceWorker" in navigator) {
