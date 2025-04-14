47dbcf12c4615ee24e996bc56429204125984d77
webcubed from home <lainathannlaiv2@gmail.com>
2025-04-14 16:33:15 -0400

    frontend functionality for authenticating

---
+++ 802/chat/auth/script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+	document
+		.querySelector("#submitbutton")
+		.addEventListener("click", async () => {
+			const email = document.querySelector("#emailinput").value;
+			document.querySelector("#codedisplay").textContent = email;
+			document.querySelector("#checkbutton").disabled = false;
+			const codeRequest = await fetch(
+				`https://recline-backend.vercel.com/genCode`,
+				{
+					method: "POST",
+					body: email,
+				}
+			).then((response) => response.json());
+			const code = codeRequest.code;
+			document.querySelector("#codedisplay").textContent = code;
+			// Store code into localstorage
+			localStorage.setItem("code", code);
+		});
+	document.querySelector("#checkbutton").addEventListener("click", async () => {
+		const code = localStorage.getItem("code");
+		const response = await fetch(`https://recline-backend.vercel.com/check`, {
+			method: "POST",
+			body: code,
+		}).then((response) => response.text());
+		if (response === "authorized :>") {
+			globalThis.location.href = `${globalThis.location.origin}/chat`;
+		}
+	});
+});
---
+++ 802/chat/script.js
@@ -0,0 +1,18 @@
+function sendMessage() {
+	const message = document.querySelector("#messageinput").value;
+	const messageElement = document.createElement("div");
+	messageElement.className = "message myMessage";
+	messageElement.innerHTML = message;
+	document.querySelector("#messages").append(messageElement);
+	fetch("https://recline-backend.vercel.com/sendMessage", {
+		method: "POST",
+		body: message,
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			const responseElement = document.createElement("div");
+			responseElement.className = "message";
+			responseElement.innerHTML = data.response;
+			document.querySelector("#messages").append(responseElement);
+		});
+}
